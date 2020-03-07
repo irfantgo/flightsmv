27e3d871800fa685ecb5f6431c3fc0b4ff6a537d
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-08 02:25:48 +0500

    New structure for configuration

--- config.php
+++ config.php
@@ -1,26 +1,14 @@
 <?php
 
+    // Bot Information
     define('BOT_TOKEN', '185576081:AAHdZ1AoiS8-RHi0HryspWZNk1KoFUUQ9Ko');
 
-
-    // Get database configuration
-    if( file_exists(__DIR__ . '/dbconfig.php') == false ) {
-        die("You forgot to create a database configuration file");
-    }
-
-    include __DIR__ . '/dbconfig.php';
-
-    // Check for composer modules
-    if( is_dir(__DIR__ . '/vendor') == false ) {
-        die("Did you install composer modules?");
-    }
-
-
-    // define('DBHOST', '127.0.0.1');
-    // define('DBUSER', 'root');
-    // define('DBPASS', 'qwerthaanu16');
-    // define('DBNAME', 'mordis');
-
-
-    // Set Timezone
-    date_default_timezone_set('Asia/Karachi');
+    // Database Configuration
+    $dbConfig = [
+        'DBHOST' => '127.0.0.1',
+        'DBUSER' => 'root',
+        'DBPASS' => 'root',
+        'DBNAME' => 'flightmv',
+        'DBPORT' => 3306
+    ];
+    
