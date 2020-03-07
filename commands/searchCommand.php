c7a1b689d73282cc54536a092acc2a8482676120
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-08 03:07:47 +0500

    Testing flight search

--- commands/searchCommand.php
+++ commands/searchCommand.php
@@ -2,6 +2,5 @@
 
     $dbConn = new MysqliDb ($dbConfig['DBHOST'], $dbConfig['DBUSER'], $dbConfig['DBPASS'], $dbConfig['DBNAME']);
 
-    $flight_no = $telegram->text;
-
-    $response_text = $flight_no;
+    $response_text = $telegram->text;
+    
