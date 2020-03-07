e4df30e9183845c52c59e1edd714caed18bf4b3f
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-08 03:08:36 +0500

    Testing flight search

--- commands/searchCommand.php
+++ commands/searchCommand.php
@@ -2,5 +2,36 @@
 
     $dbConn = new MysqliDb ($dbConfig['DBHOST'], $dbConfig['DBUSER'], $dbConfig['DBPASS'], $dbConfig['DBNAME']);
 
-    $response_text = $telegram->text;
+    $flight_no = $telegram->text;
+
+    if( empty($flight_no) ) {
+        $response_text = 'You forgot to mention the flight number';
+    }
+    else {
+
+        // Look for the flight
+        $dbConn->where('flight_no', $flight_no);
+        $flights = $dbConn->get('flightinfo');
+
+        if( empty($flights) ) {
+            $response_text = 'Sorry, I was unable to find the flight information for ' . $flight_no;
+        }
+        else {
+
+            $response_text = '';
+
+            foreach( $flights as $flight ) {
+
+                $response_text .= strtoupper($flight['direction']) . "\n";
+                $response_text .= "Date: " . $flight['scheduled_d'] . "\n";
+                $response_text .= "Time: " . $flight['scheduled_t'] . "\n"; 
+                $response_text .= "Status: " . ($flight['flight_status'] ? $flight['flight_status'] : 'Not Available') . "\n"; 
+                $response_text .= "----------------------------\n"; 
+
+            }
+
+        }
+
+    }
+
     
