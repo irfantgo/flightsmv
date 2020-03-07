3661f51a63dd2a81dbfb4b0341b4d4bb053e159f
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-08 02:30:47 +0500

    Fixed typo

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 
     // Get database configuration
     if( file_exists(__DIR__ . '/config.php') == false ) {
-        die("You forgot to create a database configuration file");
+        die("You forgot to create a database configuration file\n");
     }
     
     // Include database configuration
@@ -10,14 +10,13 @@
 
     // Check for composer modules
     if( is_dir(__DIR__ . '/vendor') == false ) {
-        die("Did you install composer modules?");
+        die("Did you install composer modules?\n");
     }
 
      // Check if bot token is set
      if( defined('BOT_TOKEN') || BOT_TOKEN == "" ) {
-        die("Telegram bot token not set");
+        die("Telegram bot token not set\n");
     }
-
 
     // Set Timezone
     date_default_timezone_set('Asia/Karachi');
