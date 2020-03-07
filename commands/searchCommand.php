e7465fdcc1a855258232e3ced5203a9cd41b909e
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-08 03:10:58 +0500

    Testing flight search

--- commands/searchCommand.php
+++ commands/searchCommand.php
@@ -9,6 +9,10 @@
     }
     else {
 
+        // Clean up flight no
+        $flight_no = str_replace(' ', '', $flight_no);
+        $flight_no = strtoupper($flight_no);
+
         // Look for the flight
         $dbConn->where('flight_no', $flight_no);
         $flights = $dbConn->get('flightinfo');
@@ -23,6 +27,8 @@
             foreach( $flights as $flight ) {
 
                 $response_text .= strtoupper($flight['direction']) . "\n";
+                $response_text .= "Airline: " . $flight['airline_name'] . "\n";
+                $response_text .= "Flight No: " . $flight['flight_no'] . "\n";
                 $response_text .= "Date: " . $flight['scheduled_d'] . "\n";
                 $response_text .= "Time: " . $flight['scheduled_t'] . "\n"; 
                 $response_text .= "Status: " . ($flight['flight_status'] ? $flight['flight_status'] : 'Not Available') . "\n"; 
