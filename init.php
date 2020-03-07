58d5b3dce4c2a4687c87c315fc6e9217d4927fad
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-08 02:29:25 +0500

    Fixed a logical error

--- init.php
+++ init.php
@@ -1,21 +1,21 @@
 <?php
 
-    // Check if bot token is set
-    if( defined('BOT_TOKEN') || BOT_TOKEN == "" ) {
-        die("Telegram bot token not set");
-    }
-
     // Get database configuration
-    if( file_exists(__DIR__ . '/dbconfig.php') == false ) {
+    if( file_exists(__DIR__ . '/config.php') == false ) {
         die("You forgot to create a database configuration file");
     }
     
     // Include database configuration
-    include __DIR__ . '/dbconfig.php';
+    include __DIR__ . '/config.php';
 
     // Check for composer modules
     if( is_dir(__DIR__ . '/vendor') == false ) {
         die("Did you install composer modules?");
+    }
+
+     // Check if bot token is set
+     if( defined('BOT_TOKEN') || BOT_TOKEN == "" ) {
+        die("Telegram bot token not set");
     }
 
 
