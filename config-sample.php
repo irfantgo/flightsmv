19f96fa9dc0130fb154b372917130c5f78570999
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-08 02:25:39 +0500

    New sample config file

---
+++ config-sample.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Bot Information
+    define('BOT_TOKEN', '');
+
+    // Database Configuration
+    $dbConfig = [
+        'DBHOST' => '127.0.0.1',
+        'DBUSER' => '',
+        'DBPASS' => '',
+        'DBNAME' => '',
+        'DBPORT' => 3306
+    ];
