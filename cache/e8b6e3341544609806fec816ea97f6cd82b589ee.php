d1831a3a444b8c25e754b5186fdd8ba8238affc4
Ahmed Shan <ahmedshan16@gmail.com>
2020-03-23 14:45:30 +0500

    Removed cache

--- cache/e8b6e3341544609806fec816ea97f6cd82b589ee.php
+++
@@ -1,9 +0,0 @@
-<section class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-12">
-                <h1 style="text-transform: uppercase"><?php echo $__env->yieldContent('page_title'); ?></h1>
-            </div>
-        </div>
-    </div><!-- /.container-fluid -->
-</section><?php /**PATH /Users/shan/www/flightsmv/views/cpanel/parts/page-header.blade.php ENDPATH**/ ?>
