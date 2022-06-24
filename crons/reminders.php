42748b9ee363b2420d3c791545c7682df71c5fc4
Ahmed Shan <ahmedshan16@gmail.com>
2022-06-24 17:07:38 +0500

    Changed to telegram bot

--- crons/reminders.php
+++ crons/reminders.php
@@ -13,7 +13,10 @@
 
     $dataset = $dbConn->rawQuery("SELECT * FROM tbl_reminders WHERE stop_reminder = 0");
 
+    // Terminate if no requests found
     if ( empty($dataset) ) { exit; }
+
+    $telegram = new Telegram(_env('TELEGRAM_BOT_TOKEN'), _env('TELEGRAM_BOT_NAME'));
 
     foreach ( $dataset as $data ) {
 
@@ -25,16 +28,16 @@
 
             // Send notification
             $msg = $flight['airline_name'] . " Flight no " . $flight['flight_no'] . " is " . $flight['flight_status'];
-            shell_exec('curl -X POST -d "{\"text\": \"'.$msg.'\"}" -H "Content-Type: application/json" https://api.pushcut.io/q5sFH41aBmrdVtXWHPCaa/notifications/MyNotification');
+            $telegram->sendMessage($data['telegram_chat_id'], $msg);
 
             $d['current_status'] = $flight['flight_status'];
 
-            if ( $flight['flight_status'] == 'CANCELLED' || $flight['flight_status'] == 'LANDED' ) {
-                $d['stop_reminder'] = 1;
-            }
+            if ( $flight['flight_status'] == 'CLOSED' )     { $d['stop_reminder'] = 1;   }
+            if ( $flight['flight_status'] == 'CANCELLED' )  { $d['stop_reminder'] = 1;   }
+            if ( $flight['flight_status'] == 'LANDED' )     { $d['stop_reminder'] = 1;   }
 
             // Update record
-            if ( $dbConn->update('tbl_reminders', $dataset) == false ) {
+            if ( $dbConn->update('tbl_reminders', $d) == false ) {
                 log_message("An error occured when trying to update record");
             }
 
